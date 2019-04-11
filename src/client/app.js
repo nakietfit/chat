eabf6a275d72a20bddff9b56407661d059241f15
Nguyen Anh Kiet <kietna@evolableasia.vn>
2019-04-11 11:36:12 +0700

    add scss & config webpack

--- src/client/app.js
+++ src/client/app.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Chat from './containers/Chat'
+import './style.scss'
 
 export default class App extends Component {
     render() {
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ const moduleObj = {
             test: /\.js$/,
             exclude: /node_modules/,
             loaders: ["babel-loader"],
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
         }
     ],
 };
