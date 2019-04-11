ab6382099032ae066681989ff31b0e58686b84b7
Nguyen Anh Kiet <kietna@evolableasia.vn>
2019-04-11 13:43:12 +0700

    mock data people list & add plugin

--- src/client/components/People/index.js
+++ src/client/components/People/index.js
@@ -4,11 +4,11 @@ export default class People extends Component {
   render() {
     return (
       <li class="clearfix">
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg" alt="avatar" />
+        <img src={this.props.people.avatar} alt="avatar" />
         <div class="about">
-          <div class="name">Vincent Porter</div>
+          <div class="name">{this.props.people.name}</div>
           <div class="status">
-            <i class="fa fa-circle online"></i> online
+            <i class="fa fa-circle online"></i> {this.props.people.status}
           </div>
         </div>
       </li>
