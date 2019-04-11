f302604097b497b5a232b65345cdc24dc99c9b85
Nguyen Anh Kiet <kietna@evolableasia.vn>
2019-04-11 16:55:32 +0700

    add signup

---
+++ src/client/components/SignUp/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class SignUp extends Component {
+  constructor(props) {
+    super(props)
+
+    this.email = React.createRef();
+    this.password = React.createRef();
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.email.current.value, this.password.current.value);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input type="email" ref={this.email} placeholder="Email" />
+        <input type="password" ref={this.password} placeholder="Password" />
+        <button>Sign up</button>
+      </form>
+    )
+  }
+}
