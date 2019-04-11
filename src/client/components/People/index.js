2d1f3516e217c519d3f3443b33834e2e09c3b1b6
Nguyen Anh Kiet <kietna@evolableasia.vn>
2019-04-11 14:10:55 +0700

    add status class

--- src/client/components/People/index.js
+++ src/client/components/People/index.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 
 export default class People extends Component {
   render() {
+    let statusClass = ''
+    if (this.props.people.status == 'online') {
+      statusClass = 'fa fa-circle online'
+    }
+    if (this.props.people.status != 'online') {
+      statusClass = 'fa fa-circle offline'
+    }
+
     return (
       <li class="clearfix">
         <img src={this.props.people.avatar} alt="avatar" />
         <div class="about">
           <div class="name">{this.props.people.name}</div>
           <div class="status">
-            <i class="fa fa-circle online"></i> {this.props.people.status}
+            <i class={statusClass}></i> {this.props.people.status}
           </div>
         </div>
       </li>
