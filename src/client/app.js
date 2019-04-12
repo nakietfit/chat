ce40e9602372498247dd809f844ef824204fc896
Nguyen Anh Kiet <kietna@evolableasia.vn>
2019-04-12 10:02:46 +0700

    add get current user api

--- src/client/app.js
+++ src/client/app.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react'
 import Chat from './containers/Chat'
 import './style.scss'
+import ParseSDK from './helpers/parseSDK'
 
 export default class App extends Component {
     render() {
+        const currentUser = ParseSDK.User.current();
+        
         return <Chat />
     }
 }
