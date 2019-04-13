b1cd46b9a5e76694b4b98edeb676a6b756e304ae
DESKTOP-6AE2BR5\ANHKIET <nakietfit@gmail.com>
2019-04-13 09:53:44 +0700

    add ref to textarea

--- src/client/components/ChatMessage/index.js
+++ src/client/components/ChatMessage/index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export default class ChatMessage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.messageToSend = React.createRef();
+  }
+
   render() {
     return (
       <div className="chat-message clearfix">
-        <textarea name="message-to-send" id="message-to-send" placeholder ="Type your message" rows="3"></textarea>
+        <textarea name="message-to-send" ref={this.messageToSend} placeholder ="Type your message" rows="3"></textarea>
                 
         <i className="fa fa-file-o"></i> &nbsp;&nbsp;&nbsp;
         <i className="fa fa-file-image-o"></i>
