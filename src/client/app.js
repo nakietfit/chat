28a879ed3e3e66fe991053c072567594f877cf0a
DESKTOP-6AE2BR5\ANHKIET <nakietfit@gmail.com>
2019-04-13 21:24:22 +0700

    add router

--- src/client/app.js
+++ src/client/app.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import Chat from './containers/Chat'
+import SignUp from './components/SignUp'
+import LogIn from './components/LogIn'
 import './style.scss'
 
 export default class App extends Component {
     render() {
-        return <Chat />
+        return (
+            <HashRouter>
+                <Switch>
+                    <Route exact path="/" component={Chat} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/login" component={LogIn} />
+                </Switch>
+            </HashRouter>
+        )
     }
 }
